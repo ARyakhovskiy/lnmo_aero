1a377f5a7d7c564eb8f82ac411e9f5bf4a6ea9a8
Alexey Ryakhovskiy <alexey.i.ryakhovskiy@gmail.com>
2019-01-19 13:04:44 -0800

    Add files via upload

--- panelize.m
+++ panelize.m
@@ -1,22 +1,19 @@
-function [alpha_u, h_u, alpha_l, h_l] = panelize(x_u, x_l, y_u, y_l)
-  alpha_u = zeros(1, numel(y_u)-1);
-  alpha_l = zeros(1, numel(y_l)-1);
-  h_u = zeros(1, numel(y_u)-1); %length between points (up)
-  h_l = zeros(1, numel(y_l)-1);%length between points (down)
-  for i=1:numel(y_u)-1
-    y1=y_u(i);
-    y2=y_u(i+1);
-      x1=x_u(i);
-    x2=x_u(i+1);
-    alpha_u(i)= FunAngle(y1,y2,x1,x2);
-    h_u(i)=Length(y1,y2,x1,x2);
-  end
-  for i =1:numel(y_l)-1
-    y1=y_l(i);
-    y2=y_l(i+1);
-    x1=x_l(i);
-    x2=x_l(i+1);
-    alpha_l(i)= FunAngle(y1,y2,x1,x2);
-    h_l(i)=Length(y1,y2,x1,x2);
-  end
+function panels = panelize(X, Y)
+  n = numel(X) - 1;
+  alpha = zeros(1, n);
+  h = zeros(1, n);
+  centerX = zeros(1, n);
+  centerY = zeros(1, n);
+  for i=1:n
+    y1=Y(i);
+    y2=Y(i+1);
+    x1=X(i);
+    x2=X(i+1);
+    alpha(i)= FunAngle(y1,y2,x1,x2);
+    h(i)=sqrt((y2-y1)^2 + (x2-x1)^2);
+    centerX(i) = (x_u(i+1) + x_u(i))/2;
+    centerY(i) = (y_u(i+1) + y_u(i))/2; 
+    panels(i) = struct('centerX', centerX(i), 'centerY', centerY(i), 'angle', alpha(i), 'length', h(i));
+    end
 endfunction
+
