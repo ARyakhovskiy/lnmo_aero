0cf00e97c09d7669908418b6cf831c2c6d23a88f
Vsevolode <45514162+Vsevolode@users.noreply.github.com>
2019-01-20 10:22:23 +0300

    Delete Integral_S.m

--- Integral_S.m
+++
@@ -1,9 +0,0 @@
-function I = Integral_S (x, y)
-  if (numel(x) == numel(y))
-   n = numel(x);
-  else
-   error("Orders of arrays do not match");
- end
- I=0;
- I=((x(n)-x(1))/6)*(y(1)+4*y((1+n)/2)+y(n));
-end
